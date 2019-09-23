e443ed08349ccea19ef43d055585e36160be25c7
tyzo4ka <tyzo4ka@gmail.com>
2019-09-23 15:18:31 +0600

    added forms.py

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,7 +1,12 @@
-# from django import forms
-#
-#
-# class ProductForm(forms.Form):
-#     author = forms.CharField(max_length=40, required=True, label='Author')
-#     email = forms.EmailField(max_length=50, required=True, label='Email')
-#     text = forms.CharField(max_length=3000, required=True, label="Text")
+from django import forms
+from django.forms import widgets
+from webapp.models import CATEGORY_CHOICES
+
+
+class ProductForm(forms.Form):
+    name = forms.CharField(max_length=40, required=True, label='name')
+    description = forms.CharField(max_length=3000, required=True, label="Description")
+    category = forms.ChoiceField(choices=CATEGORY_CHOICES, required=False, label='Category')
+    remainder = forms.IntegerField(min_value=1, required=True, label="Amount")
+    price = forms.DecimalField(max_digits=7, decimal_places=2, required=True, label="Price")
+
